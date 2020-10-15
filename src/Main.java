3e97bbc064089e72d40c2360d7f691415ac05949
Sophie Kadletz <sophie.kadletz@htl-wels.at>
2020-10-15 13:00:21 +0200

    Label added

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
 import javafx.application.Application;
-import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
@@ -19,13 +17,23 @@ public class Main extends Application {
     @Override
     public void start(Stage stage){
         VBox box_main = new VBox();
-        box_main.setPadding(new Insets(500));
+        Scene scene = new Scene(box_main,500,500);
         box_main.setBackground(new Background (new BackgroundFill(Color.CORNFLOWERBLUE, CornerRadii.EMPTY, Insets.EMPTY)));
 
+        Label label_celsius = new Label("Grad Celsius");
+        label_celsius.setMaxWidth(200);
+        label_celsius.setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)));
+        label_celsius.setStyle("-fx-font-size:20;-fx-font-weight: bold");
+
+        Label label_fahrenheit = new Label("Grad Fahrenheit");
+        label_fahrenheit.setMaxWidth(200);
+        label_fahrenheit.setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)));
+        label_fahrenheit.setStyle("-fx-font-size:20;-fx-font-weight: bold");
 
 
-        Scene scene = new Scene(box_main,500,500);
-        stage.setTitle("");
+        box_main.getChildren().addAll(label_celsius,label_fahrenheit);
+
+        stage.setTitle("Temp. Converter");
         stage.setScene(scene);
         stage.show();
     }
